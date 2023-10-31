f271f931fde068c67f2f3517b546c784e9fd4ce3
Matt Mulheran <matt.mulheran@rapidplatform.com.au>
2023-10-31 14:16:38 +1000

    chore: update rollup and package json config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,6 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
-import copy from 'rollup-plugin-copy'
 import dts from 'rollup-plugin-dts'
 import external from 'rollup-plugin-peer-deps-external'
 
@@ -14,13 +13,11 @@ export default [
         file: 'dist/cjs/index.js',
         format: 'cjs',
         assetFileNames: '[name][extname]',
-        sourcemap: true,
       },
       {
         file: 'dist/esm/index.js',
         format: 'esm',
         assetFileNames: '[name][extname]',
-        sourcemap: true,
       },
     ],
     plugins: [
@@ -28,12 +25,9 @@ export default [
       resolve(),
       commonjs(),
       typescript({
-        exclude: ['**/*.stories.*', '**/*.test.*'],
+        exclude: ['**/*.test.*'],
       }),
       terser(),
-      copy({
-        targets: [{ src: 'src/styles.css', dest: 'dist' }],
-      }),
     ],
   },
   {
